8996099ea6039a086e8e60b974f9b8ef9a2bb751
Situ William <situ_william@yahoo.com>
2017-11-16 16:25:29 -0500

    finished most of the styling

--- crm.rb
+++ crm.rb
@@ -2,17 +2,18 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+	@contact_list = Contact.all()
 	erb :index
 end
 
 get '/contacts' do
-	@contact_list = Contact.all
 	erb :contacts
 end
 
 get '/about' do
 	erb :about
 end
+
 after do
 	ActiveRecord::Base.connection.close
 end
